4d3bdbaad7b784fc78077d046039fb5d34415a05
iOSCS <dbrckstla@naver.com>
2018-08-30 14:14:07 +0900

    프로젝트 생성

---
+++ Sources/CollectionViewLayout.swift
@@ -0,0 +1,9 @@
+//
+//  CollectionViewLayout.swift
+//  CollectionViewLayout
+//
+//  Created by Chansim Youk on {TODAY}.
+//  Copyright © 2018 CollectionViewLayout. All rights reserved.
+//
+
+import Foundation
---
+++ Tests/CollectionViewLayoutTests/CollectionViewLayoutTests.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewLayoutTests.swift
+//  CollectionViewLayout
+//
+//  Created by Chansim Youk on {TODAY}.
+//  Copyright © 2018 CollectionViewLayout. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import CollectionViewLayout
+
+class CollectionViewLayoutTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //// XCTAssertEqual(CollectionViewLayout().text, "Hello, World!")
+    }
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
